e50dfa3bdf408c21a83c228d29407a98e4944978
sunJ0120 <sspure1214@gmail.com>
2025-11-29 14:41:34 +0900

    study: 싱글톤 구현 방식4 : Double checked locking pattern을 이용해서 싱글톤 구현하기

---
+++ design-patterns/practice/singleton/Singleton3.java
@@ -0,0 +1,20 @@
+package singleton;
+
+public class Singleton3 {
+    private static volatile Singleton3 instance;
+
+    private Singleton3() {
+
+    }
+
+    public static Singleton3 getInstance() {
+        if (instance == null) {
+            synchronized (Singleton3.class) {
+                if (instance == null) {
+                    instance = new Singleton3();
+                }
+            }
+        }
+        return instance;
+    }
+}
--- design-patterns/practice/singleton/main.java
+++ design-patterns/practice/singleton/main.java
@@ -1,10 +1,11 @@
 import singleton.Singleton1;
 import singleton.Singleton2;
+import singleton.Singleton3;
 
 public class main {
     public static void main(String[] args) {
-        Singleton2 s1 = Singleton2.getInstance();
-        Singleton2 s2 = Singleton2.getInstance();
+        Singleton3 s1 = Singleton3.getInstance();
+        Singleton3 s2 = Singleton3.getInstance();
 
         System.out.println(s1 == s2);    // true
     }
