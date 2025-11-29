c02723d156e34442eeb0192b8d5a14a9e3e62d86
sunJ0120 <sspure1214@gmail.com>
2025-11-29 09:29:21 +0900

    study: 싱글톤 구현 방식1 : 생성자 private, get 메서드 사용
    - 이 방식의 경우, thread-safe 하지 않아서 멀티 쓰레드 환경에서 문제가 된다.
    - JVM이 메서드 단위의 thread-safe를 보장하지 않기 때문이다.

--- design-patterns/practice/singleton/Singleton1.java
+++ design-patterns/practice/singleton/Singleton1.java
@@ -1,4 +1,17 @@
 package singleton;
 
 public class Singleton1 {
+    private static Singleton1 instance;
+
+    private Singleton1() {
+    }
+
+    // >> 2번 쓰레드 대기
+    public static synchronized Singleton1 getInstance() {
+        // >> 1번 쓰레드 도착
+        if (instance == null) {
+            instance = new Singleton1();
+        }
+        return instance;
+    }
 }
