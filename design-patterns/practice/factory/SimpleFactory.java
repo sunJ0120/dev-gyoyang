3d7eba747e8ad1e06c667b11d7d655c8016c09e7
sunJ0120 <sspure1214@gmail.com>
2025-12-01 20:13:32 +0900

    study: 팩토리 메서드 패턴 - 실무에 활용되는 심플 팩토리 메서드 패턴 예시

---
+++ design-patterns/practice/factory/SimpleFactory.java
@@ -0,0 +1,12 @@
+package factory;
+
+public class SimpleFactory {
+    public Object createProduct(String name) {
+        if (name.equals("whiteship")) {
+            return new WhiteShip();
+        } else if (name.equals("blackship")) {
+            return new BlackShip();
+        }
+        throw new IllegalArgumentException();
+    }
+}
